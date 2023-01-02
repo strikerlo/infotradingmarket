10a88e836f14b620fa1aebaf47011c32a63bae47
strikerlo <115256397+strikerlo@users.noreply.github.com>
2023-01-02 08:19:11 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const con = document.getElementById('menu-btn');
+const nav = document.querySelector('.navbar')
+
+con.addEventListener('click', () => {
+    nav.classList.toggle('show');
+
+});
+
+
+
+
+
+
+
+
+
+
+
